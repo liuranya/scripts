e2a809a3a6d030222234f931595e356645862bb6
liuranya <52848742+liuranya@users.noreply.github.com>
2022-05-21 22:00:16 +0800

    Update jd_superBrand.js

--- jd_superBrand.js
+++ jd_superBrand.js
@@ -1,11 +1,10 @@
-/*
- 特务集卡
+/**
+ 特务Z
  脚本没有自动开卡，会尝试领取开卡奖励
-cron:35 10,18,20 * * *
-
-35 10,18,20 * * * jd_superBrand.js
-* */
-const $ = new Env('特务Z-II');
+ cron 23 8,9 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_productZ4Brand.js
+ 一天要跑2次
+ */
+const $ = new Env('特务Z');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let cookiesArr = [];
